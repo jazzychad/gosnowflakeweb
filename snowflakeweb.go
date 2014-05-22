a9c4b43ac9e68bfc9f2d3dd589351cc5baf2f4de
Chad Etzel <chad@jazzychad.net>
2014-05-22 13:05:20 -0700

    code cleanup

--- snowflakeweb.go
+++ snowflakeweb.go
@@ -4,13 +4,41 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"jc/snowflakeweb/snowflake"
 )
 
+const workers uint64 = 5
+var requestChannel [workers]chan (chan uint64)
+var responseChannel chan uint64
+var currentWorker int
+
+func startSnowflake(datacenterId uint64, workerId uint64) {
+	s := snowflake.NewSnowflake(datacenterId, workerId)
+	requestChannel[workerId] = make(chan (chan uint64))
+
+	for {
+		responseChan := <- requestChannel[workerId]
+		fmt.Println(workerId, "handling request")
+		id := s.NextId()
+		fmt.Println("id:", id)
+		responseChan <- id
+	}
+	
+}
 
 func main() {
 
-	http.HandleFunc("/", hello)
+	currentWorker = 0
+	responseChannel = make(chan uint64)
+	datacenterId, _ := strconv.ParseUint(os.Getenv("DATACENTER_ID"), 0, 64)
+	var i uint64
+
+	for i = 0; i < workers; i++ {
+		go startSnowflake(datacenterId, i)
+	}
+
+	http.HandleFunc("/", handler)
 	fmt.Println("listening...")
 	port := os.Getenv("PORT")
 	err := http.ListenAndServe(":" + port, nil)
@@ -19,8 +47,12 @@ func main() {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	s := snowflake.NewSnowflake(1, 1)
-	//fmt.Println(s.NextId())
-	fmt.Fprintln(res, s.NextId())
- }
+func handler(res http.ResponseWriter, req *http.Request) {
+
+	requestChannel[currentWorker] <- responseChannel
+	currentWorker = (currentWorker + 1) % int(workers)
+
+	id := <- responseChannel
+	fmt.Fprintln(res, id)
+
+}
