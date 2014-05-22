1aef2f2c0cfe4d95382821e7961faa6e7557c96d
Chad Etzel <chad@jazzychad.net>
2014-05-22 11:12:23 -0700

    first commit

---
+++ snowflakeweb.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"pkg/snowflake"
+)
+
+
+func main() {
+
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	port := os.Getenv("PORT")
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	s := snowflake.NewSnowflake(1, 1)
+	//fmt.Println(s.NextId())
+	fmt.Fprintln(res, s.NextId())
+ }
